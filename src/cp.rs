9586bb27143408ad30eda62159d5d6b20bf9e158
avinashpai <paiavinash200@gmail.com>
2021-08-07 11:29:50 -0700

    cp working for files w progress

--- src/cp.rs
+++ src/cp.rs
@@ -3,40 +3,48 @@
  * */
 
 use std::{
-    fs,
-    io::{self, copy, Read},
+    fs::{self, File},
+    io::{self, copy},
     path::Path,
 };
 
-use std::thread;
-
 use indicatif::{ProgressBar, ProgressStyle};
 
-struct CopyProgress<T> {
-    inner: T,
-    progress_bar: ProgressBar,
-}
-
-impl<T: Read> Read for CopyProgress<T> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.inner.read(buf).map(|n| {
-            self.progress_bar.inc(n as u64);
-            n
-        })
-    }
-}
+use crate::util;
 
 pub fn cp(path: &Path) -> io::Result<()> {
-    let bar = ProgressBar::new(100);
-    bar.set_style(
-        ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
-            .progress_chars("##-"),
-    ); // basic styling as example
-    for _ in 0..100 {
-        bar.inc(1);
-        thread::sleep(std::time::Duration::from_millis(10));
-    }
-    bar.finish();
+    let metadata = fs::metadata(path)?;
+
+    if metadata.file_type().is_file() {
+        return cp_file(path, metadata);
+    } else if metadata.file_type().is_dir() {
+        return recur_dir(path);
+    } // TODO: symlinks
+
     Ok(())
+}
+
+fn cp_file(path: &Path, metadata: fs::Metadata) -> io::Result<()> {
+    let progress_bar = ProgressBar::new(metadata.len());
+    progress_bar.set_style(ProgressStyle::default_bar()
+                .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+                .progress_chars("#>-"));
+
+    let mut source = File::open(path)?;
+
+    let mut file_path = util::get_dpbx_path()?;
+    file_path.push(path.file_name().unwrap());
+
+    let dest = File::create(file_path.as_path())?;
+
+    println!("Copying {:?} to {:?}...", path, file_path);
+    let _ = copy(&mut source, &mut progress_bar.wrap_write(dest))?;
+
+    progress_bar.finish();
+
+    Ok(())
+}
+
+fn recur_dir(path: &Path) -> io::Result<()> {
+    todo!()
 }
